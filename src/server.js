f4844052b4dee204669af3238a880f2904817816
Hyun-h <hyeonahong@gmail.com>
2022-06-27 22:48:50 +0900

    소켓 연결

--- src/server.js
+++ src/server.js
@@ -12,6 +12,14 @@ app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
+//http 서버와 webSocket 서버를 같이 돌리지 않아도 괜찮음. 필요에 따라 ws만 사용
+//이렇게 하면 localhost는 동일한 포트에서 http, ws 두 개의 protocol 사용 가능
 const server = http.createServer(app);
-
 const wss = new WebSocket.Server({ server });
+
+function handleConnection(socket) {
+  console.log(socket);
+}
+wss.on("connection", handleConnection);
+
+server.listen(3000, handleListen);
